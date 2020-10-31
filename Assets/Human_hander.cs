d7f7d14dc959326e6660f94089cca7e43d551326
AnthonyLIU98 <71893308+AnthonyLIU98@users.noreply.github.com>
2020-10-31 19:44:53 +0800

    Session-2

---
+++ Assets/Human_hander.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RC3_9_Humans;
+using RC3_9_Cyborgs;
+
+public class Human_hander : MonoBehaviour
+{
+
+    Human_Class yiguan, phil, yiguan2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yiguan = new Human_Class("Yiguan Liu", 1.74f);
+        phil = new Human_Class("Phil", 1.75f);
+        yiguan2 = new Cyborg("Cyyiguan", 1.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(yiguan.GetName());
+        yiguan.Walking();
+        yiguan.WorkOur();
+    }
+}
---
+++ Cyborg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RC3_9_Humans;
+namespace RC3_9_Cyborgs
+{
+    class Cyborg : Human_Class
+    {
+        //Variables
+        string name;
+        float height;
+
+        //constructors
+        public Cyborg(string _name, float _height)
+        {
+            name = _name;
+            height = _height;
+        }
+        //behaviours
+        public void Roll()
+        {
+            //Action here
+        }
+
+    }
+
+}
